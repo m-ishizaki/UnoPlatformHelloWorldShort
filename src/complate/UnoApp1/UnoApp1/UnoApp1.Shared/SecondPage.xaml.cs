39945505ca9add32dfff9253fd52300da1723bb2
m ishizaki <love.ms.is12t@hotmail.co.jp>
2020-06-04 21:12:30 +0900

    complete second page.

--- src/complate/UnoApp1/UnoApp1/UnoApp1.Shared/SecondPage.xaml.cs
+++ src/complate/UnoApp1/UnoApp1/UnoApp1.Shared/SecondPage.xaml.cs
@@ -25,6 +25,22 @@ namespace UnoApp1.Shared
         public SecondPage()
         {
             this.InitializeComponent();
+
+#if __WASM__
+            platform.Text = "WASM";
+#endif
+
+#if WINDOWS_UWP
+           platform.Text = "UWP";
+#endif
+
+#if __ANDROID__
+            platform.Text = "Android";
+#endif
+
+#if __IOS__
+           platform.Text = "iOS";
+#endif
         }
 
 
