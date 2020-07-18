600b18345d1e3d978a19c90205cd76b4fff2edc6
m ishizaki <love.ms.is12t@hotmail.co.jp>
2020-07-18 11:41:35 +0900

    update complate

--- src/complate/UnoApp1/UnoApp1/UnoApp1.Shared/SecondPage.xaml.cs
+++ src/complate/UnoApp1/UnoApp1/UnoApp1.Shared/SecondPage.xaml.cs
@@ -1,25 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
 
 namespace UnoApp1.Shared
 {
-    /// <summary>
-    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
-    /// </summary>
     public sealed partial class SecondPage : Page
     {
         public SecondPage()
@@ -42,7 +26,6 @@ namespace UnoApp1.Shared
            platform.Text = "iOS";
 #endif
         }
-
 
         private void Button_Click_GoBack(object sender, RoutedEventArgs e)
         {
